50e8b7e011a07343f23080721c2f00bb5061a73c
Margaret Wismer <margaretgwismer.gmail.com>
2016-12-06 17:08:31 -0500

    Displays boxes

--- main.js
+++ main.js
@@ -1,76 +1,101 @@
-listOfPhrases = ["Democratic process", "Constitutional rights",
-"Executive power", "Legislative branch"];
+$(document).ready(function() {
+  listOfPhrases = ["Democratic process", "Constitutional rights",
+  "Executive power", "Legislative branch"];
 
-var currentGame = new wordGame("Wheel of Fortune");
-currentGame.start();
+  var currentGame = new wordGame("Wheel of Fortune");
+  currentGame.start();
 
-//Top object wordGame
-function wordGame(title) {
-  this.name = title;
-  this.start = function() {
-    this.displayStartPage();
-  }
-  
-  this.playGame = function() {  
-    quote = this.selectQuote();
-    board = new makeBoard(phrase);
-    board.displayGamePage();
-    board.play()
-  }
-
-  //this function part of wordGame object
-  this.displayStartPage = function() {
-    $("start-div").css("visibility", "visible");
-    $("start-btn").click(function(){
-      currentGame.playGame();
-    });
-  }
-
-  this.selectQuote = function() {
-    var rand = listOfPhrases[Math.floor(Math.random()*listOfPhrases.length)];
-    return rand;  
-  }
-}// End wordGame object
-
-//Main object makeBoard.
-function makeBoard(phraseString) {
-   this.phrase = phraseString;
-   this.numGuess = 0;
-   this.numLines = 0;
-   this.charPerLine = 20;
-   this.displayGamePage = function() {
-     currPhrase = new newPhrase(this.phrase);
-     this.displayWordBoxes(currPhrase);
-     this.displayGameButtons();
-     this.makeEventListeners();
-   }
-
-   this.displayWordBoxes = function(phrase) {
-     for (var i = 0; i < phrase.wordArray.length; i++) {
-        placeWordOnLine(phrase.wordArray[i],this.charPerLine);
-        createSpace(this.charPerLine);
-     }
-   }
- 
-   //can I use function displayWordBoxes() {}
-   placeWordOnLine = function(word,numSpaces) {
-    for (var i = 0; i < word.length; i++) {
-      var letBox = document.createElement("div");
-      letBox.className = "input-div-letter";
-      var letDiv = document.getElementsByClassName("input-div")[0];
-      letDiv.appendChild(letBox);
+  //Top object wordGame
+  function wordGame(title) {
+    this.name = title;
+    this.start = function() {
+      this.displayStartPage();
     }
-    this.charPerLine -= word.length;
-  }
+    
+    //this function part of wordGame object
+    this.displayStartPage = function() {
+      $(".start-div").css("visibility", "visible");
+      $(".start-btn").click(function(){
+        console.log("button clicked");
+        currentGame.playGame();
+      });
+    }
 
-   createSpace = function() {
-    var letBox = document.getElementsByClassName('input-div-letter');
-    letBox.style.marginRight = "50px";  
-   }
-} //end makeBoard object
+    this.playGame = function() {  
+      quote = this.selectQuote();
+      console.log(quote);
+      board = new makeBoard(quote);
+      board.displayGamePage();
+      //board.play()
+    }
 
-//Third object newPhrase
-function newPhrase(phrase) {
-   this.total = phrase.length;
-   this.wordArray = phrase.split(" ");
-} //end newPhrase object
+    this.selectQuote = function() {
+      console.log("phrase 1 "+listOfPhrases[0]);
+      var rand = listOfPhrases[Math.floor(Math.random()*listOfPhrases.length)];
+      console.log(rand);
+      return rand;  
+    }
+  }// End wordGame object
+
+  //Main object makeBoard.
+  function makeBoard(phraseString) {
+     this.phrase = phraseString;
+     this.numGuess = 0;
+     this.numLines = 0;
+     this.charPerLine = 20;
+     this.displayGamePage = function() {
+       currPhrase = new newPhrase(this.phrase);
+       this.displayWordBoxes(currPhrase);
+       this.displayGameButtons();
+       this.makeListenersOnPlay();
+     }
+
+     this.displayWordBoxes = function(phrase) {
+       $(".start-div").css("visibility","hidden");
+       for (var i = 0; i < phrase.wordArray.length; i++) {
+          console.log("Word "+i+" "+phrase.wordArray[i]);
+          this.placeWordOnLine(phrase.wordArray[i],this.charPerLine);
+          this.createSpace(this.charPerLine);
+       }
+       $(".input-div").css("visibility","visible");
+     }
+   
+     //can I use function displayWordBoxes() {}
+     this.placeWordOnLine = function(word,numSpaces) {
+      console.log("word length "+word.length);
+      for (var i = 0; i < word.length; i++) {
+        var letBox = document.createElement("div");
+        letBox.className = "input-div-letter";
+        var letDiv = document.getElementsByClassName("input-div")[0];
+        letDiv.appendChild(letBox);
+      }
+      this.charPerLine -= word.length;
+    }
+
+     this.createSpace = function() {
+      var space = document.createElement("div");
+      space.className = "input-div-blank";
+      var letDiv = document.getElementsByClassName("input-div")[0];
+      letDiv.appendChild(space);
+      board.charPerLine -= 1;
+      console.log("char left "+this.charPerLine);
+     }
+
+     this.displayGameButtons = function() {
+       //function to display Pick a letter
+       //make a guess
+     }
+
+     this.makeListenersOnPlay = function() {
+       //addEventListeners to Game Buttons
+     }
+
+  } //end makeBoard object
+
+  //Third object newPhrase
+  function newPhrase(phrase) {
+     this.total = phrase.length;
+     this.wordArray = phrase.split(" ");
+  } //end newPhrase object
+
+})
